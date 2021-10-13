6c3d185586e03c4696f535b5e24045b385f6cc3e
YinWei <cba5530@gmail.com>
2021-10-13 20:42:51 +0800

    新增 Post order、POST order/cancel

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,5 @@
 const db = require('../models')
-const Order = db.Order
-const Product = db.Product
+const { Order, Product, OrderItem, Cart } = db
 
 let orderController = {
   getOrders: async (req, res) => {
@@ -10,6 +9,48 @@ let orderController = {
     orders = JSON.parse(JSON.stringify(orders))
     await res.render('orders', {
       orders: orders
+    })
+  },
+  postOrder: (req, res) => {
+    return Cart.findByPk(req.body.cartId, { include: 'items' }).then(cart => {
+      return Order.create({
+        name: req.body.name,
+        address: req.body.address,
+        phone: req.body.phone,
+        shipping_status: req.body.shipping_status,
+        payment_status: req.body.payment_status,
+        amount: req.body.amount,
+      }).then(order => {
+
+        var results = [];
+        for (var i = 0; i < cart.items.length; i++) {
+          console.log(order.id, cart.items[i].id)
+          results.push(
+            OrderItem.create({
+              OrderId: order.id,
+              ProductId: cart.items[i].id,
+              price: cart.items[i].price,
+              quantity: cart.items[i].CartItem.quantity,
+            })
+          );
+        }
+
+        return Promise.all(results).then(() =>
+          res.redirect('/orders')
+        );
+
+      })
+    })
+  },
+  cancelOrder: (req, res) => {
+    return Order.findByPk(req.params.id, {}).then(order => {
+      order.update({
+        ...req.body,
+        shipping_status: '-1',
+        payment_status: '-1',
+      }).then(order => {
+        return res.redirect('back')
+      })
     })
   },
 }
